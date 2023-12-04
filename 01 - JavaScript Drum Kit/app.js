5cdef0504cc91b4f7f8ffbab2d01a2444e1a7ac8
Sandesh Shrestha <iamsandeshstha@gmail.com>
2023-12-04 19:13:35 +0545

    Added a resetter to allow for a key to play sound multiple times when its pressed

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,5 +1,6 @@
 window.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if (!audio) return;
+  audio.currenTime = 0; // to reset the play fucntion and play a sound multiple times before it ends playing
   audio.play();
 });
