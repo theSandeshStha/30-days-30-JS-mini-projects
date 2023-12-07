c47fc0cdcd8c1c030f8c1077534622f292590dfa
Sandesh Shrestha <iamsandeshstha@gmail.com>
2023-12-07 21:41:28 +0545

    Divided the HTML, CSS and JS code into their files

---
+++ 02 - JS and CSS Clock/app.js
@@ -0,0 +1,21 @@
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+
+  const sec = now.getSeconds();
+  const secHand = (sec / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secHand}deg)`;
+
+  const min = now.getMinutes();
+  const minHand = (min / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minHand}deg)`;
+
+  const hr = now.getHours();
+  const hrHand = (hr / 12) * 360 + 90; //12 placeholders for an hour, unlike 60 for secs and mins
+  hourHand.style.transform = `rotate(${hrHand}deg)`;
+}
+
+setInterval(setDate, 1000);
